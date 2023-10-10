27bc84da60701470252f1d3701961c8ad06e7856
alexandreoliv29 <alexandre.foj@gmail.com>
2023-10-10 09:25:25 -0300

    adding changess

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,14 @@
-import { useEffect, useState } from "react";
-
 
 export const Navbar = () => {
 
-
-    const [scrolling, setScrolling] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setScrolling(true);
-            } else {
-                setScrolling(false);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-
     return (
-        <nav className="navbar navbar-expand-lg position-fixed w-100 z-1 pt-2" style={{ backgroundColor: scrolling ? '#d9d9d9' : 'transparent' }}>
+        <nav className="navbar navbar-expand-lg  w-100 z-1 pt-2">
             <div className="container">
                 <a className="navbar-brand" href="#">
                     <img src="https://qria-tech.s3.amazonaws.com/LogoQro.svg" className="img-fluid" width={50} alt="" />
                 </a>
-                <div className="collapse navbar-collapse d-md-flex justify-content-center" id="navbarNav">
-                    <ul className="navbar-nav">
+                <div className="collapse navbar-collapse d-md-flex justify-content-end " id="navbarNav">
+                    <ul className="navbar-nav radius-39 gap-4 p-2 me-5" style={{backgroundColor: 'rgba(255, 255, 255, 0.10)'}}>
                         <li className="nav-item">
                             <a className="nav-link nav-link text-white ff-bold fs-6" aria-current="page" href="#">Home</a>
                         </li>
